6435143d2e30ed37cac2c8b72e7d7df6aff0f0f9
Mariela Vallecillo <mvalletelt@gmail.com>
2020-06-26 18:06:41 -0300

    arregle que reconozca todos los usuarios registrados y guarde en session

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,87 +3,81 @@ const path=require('path')
 const bcrypt = require('bcrypt');
 const { check, validationResult, body} = require ('express-validator');
 
-let usersController = {
- 
-  archivo: path.join(__dirname,'../data/users.json'),//'..') + '/data/users.json',
 
-crear: function(req, res){
+
+
+let usersController = {
+  
+  
+  archivo: path.join(__dirname,'../data/users.json'),//'..') + '/data/users.json',
+  
+  crear: function(req, res){
     usuario={
       nombre:req.body.nombre,
       email:req.body.email,
       contraseña:bcrypt.hashSync (req.body.contraseña, 10),
       avatar: req.files[0].filename  
-  }
+    }
+    
+    let errors = validationResult(req);
+    
+    if (errors.isEmpty()){
+      
+      let archivoUsuarios = fs.readFileSync(usersController.archivo, {encoding:'utf-8'});
+      let usuarios;
+      if (archivoUsuarios==''){
+        usuarios=[]
+      }else{
+        usuarios=JSON.parse(archivoUsuarios)
+      }
+      usuarios.push(usuario);
+      let usuariosJson=JSON.stringify(usuarios);
+      fs.writeFileSync(usersController.archivo,usuariosJson);
+      
+      res.render('profile', {usuario});
+    }
+    
+    else {return res.render('index',{errors:errors.errors})}
+    
+  },
   
-  let errors = validationResult(req);
+  login:function(req,res,next){
+    let errors = validationResult(req);
+    
+    if (errors.isEmpty()){
+      
+      let archivoUsuarios = fs.readFileSync(usersController.archivo, {encoding:'utf-8'});
+      let usuarios;
+      if( archivoUsuarios == ' '){
+        usuarios=[];
+      }else{
+        usuarios = JSON.parse(archivoUsuarios);
+      }
+      for (let i=0; i< usuarios.length; i++){
+        let usuario=usuarios[i]
+        if (req.body.email== usuario.email && bcrypt.compareSync(req.body.contraseña, usuario.contraseña)){
+          
+          
+          req.session.usuarioLogueado=usuario;
+          res.send('Estas logueado '+ req.session.usuarioLogueado.email )
+          
+        }
+        return res.render('index',{errors:[{msg:'Credenciales inválidas'}]});
 
-  if (errors.isEmpty()){
- 
-  let archivoUsuarios = fs.readFileSync(usersController.archivo, {encoding:'utf-8'});
-  let usuarios;
-  if (archivoUsuarios==''){
-      usuarios=[]
-   }else{
-      usuarios=JSON.parse(archivoUsuarios)
-   }
-  usuarios.push(usuario);
-  let usuariosJson=JSON.stringify(usuarios);
-  fs.writeFileSync(usersController.archivo,usuariosJson);
-
-  res.render('profile', {usuario});
-}
-
-else {return res.render('index',{errors:errors.errors})}
-
-},
-
-login:function(req,res,next){
-  let errors = validationResult(req);
-
-  if (errors.isEmpty()){
-
-  let archivoUsuarios = fs.readFileSync(usersController.archivo, {encoding:'utf-8'});
-  let usuarios;
-
-  if( archivoUsuarios == ' '){
-    usuarios=[];
-  }else{
-    usuarios = JSON.parse(archivoUsuarios);
-  }
-  for (let i=0; i< usuarios.length; i++){
-    let usuario=usuarios[i]
-    if (req.body.email== usuario.email && bcrypt.compareSync(req.body.contraseña, usuario.contraseña)){
-   
-    res.send('te encontre')
-
- }else{
- 
-   
-  return res.render('index',{errors:[{msg:'Credenciales inválidas'}]})
-}
-
-}
-
-}else{
-
- return res.render('index',{errores:errores.errors})
+      }
+      
+      
+    }else{
+      
+      return res.render('index',{errors:errors.errors})
+      
+    }
+  
+  
+  
+  
+  }  
 }
 
 
-
-}
-}
 module.exports=usersController;
-
-
-
-
-
-
-
-
- 
- 
- 
- 
- 
