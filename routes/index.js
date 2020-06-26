3afda6a020068f26247422373354089fd8efd43d
Mariela Vallecillo <mvalletelt@gmail.com>
2020-06-26 18:13:27 -0300

    guardo el logueo en session y lo compruebo en la ruta /check

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ const indexController = require('../controllers/indexController');
 /* GET home page. */
 router.get('/', indexController.raiz);  
 
+router.get('/check',function(req,res){
+ if(req.session.usuarioLogueado==undefined){
+     res.send('No estás logueado')
+ }else{
+
+    res.send('Estás logueado ' + req.session.usuarioLogueado.email)
+ }
+
+
+})
+
 module.exports = router;
